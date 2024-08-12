a5666be0a276ae72d731652ad10f5be2660ef5fa
Sreesanth Sivan ssreesanth@gmail.com <ssreesanth@gmail.com>
2024-08-12 14:58:22 +0200

    added code to stupid coach

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,11 @@
+class QuestionsController < ApplicationController
+
+def ask
+end
+
+def answer
+@answers = ["Silly question, get dressed and go to work!.", "Great!! Get some work done", "I don't care, get dressed and go to work!"]
+end
+
+
+end
